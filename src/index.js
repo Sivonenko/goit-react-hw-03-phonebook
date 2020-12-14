4e98f841a226dc7fcf1f144f4d661935037ae486
ulia <sivonenkoyulia@gmail.con>
2020-12-15 01:25:47 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'modern-normalize/modern-normalize.css';
+import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
